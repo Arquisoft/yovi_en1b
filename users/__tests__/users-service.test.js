0ea7bfbc53a6b220b5631ea040372b38658c8249
Manolo Lama <UO300896@uniovi.es>
2026-02-26 13:08:06 +0100

    increased timeout limit to 60s

--- users/__tests__/users-service.test.js
+++ users/__tests__/users-service.test.js
@@ -8,26 +8,23 @@ let mongoServer;
 
 describe('POST /createuser', () => {
 
-
     beforeAll(async () => {
         mongoServer = await MongoMemoryServer.create();
         const mongoUri = mongoServer.getUri();
-
 
         if (mongoose.connection.readyState !== 0) {
             await mongoose.disconnect();
         }
 
         await mongoose.connect(mongoUri);
-    });
-
+    }, 60000);
 
     afterAll(async () => {
         await mongoose.disconnect();
         if (mongoServer) {
             await mongoServer.stop();
         }
-    });
+    }, 60000);
 
     afterEach(() => {
         vi.restoreAllMocks()
@@ -39,13 +36,9 @@ describe('POST /createuser', () => {
             .send({ username: 'Pablo' })
             .set('Accept', 'application/json')
 
-
         expect(res.status).toBe(200)
-
-
         expect(res.body).toHaveProperty('message')
         expect(res.body.message).toMatch(/Hello Pablo! Welcome to the course!/i)
-
 
         expect(res.body).toHaveProperty('user')
         expect(res.body.user.username).toBe('Pablo')
