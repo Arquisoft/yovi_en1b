f7c0a0a4f1be87b0bafd03ac270267b3dee3c8a3
Th0be <th0betheth0be@gmail.com>
2026-03-10 20:50:26 +0100

    fix: Homepage of logged user

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -8,18 +8,22 @@ import GamePage from './pages/GamePage';
 import GameHistoryPage from './pages/GameHistoryPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import TopBar from './components/TopBar';
+import { useAuth } from './hooks/useAuth';
 
 function App() {
+  const { isLoggedIn, userId } = useAuth();
+  const profileId = userId ?? 'demo';
+
   return (
     <div className="app-shell">
       <TopBar />
 
       <main className="app-content">
         <Routes>
-          <Route path="/" element={<EntryPage />} />
+          <Route path="/" element={isLoggedIn ? <HomePage userId={profileId} /> : <EntryPage />} />
 
           <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/home" element={<HomePage userId={profileId} />} />
             <Route path="/users/:id" element={<UserDetailPage />} />
             <Route path="/users/:id/history" element={<GameHistoryPage />} />
             <Route path="/games/new" element={<NewGamePage />} />
@@ -34,7 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
--- webapp/src/pages/HomePage.tsx
+++ webapp/src/pages/HomePage.tsx
@@ -1,3 +1,22 @@
-export default function HomePage() {
-  return <h1>Homepage</h1>;
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+
+type HomePageProps = {
+  userId: string;
+};
+
+export default function HomePage({ userId }: HomePageProps) {
+  return (
+    <div className="home-actions">
+      <h1>Homepage</h1>
+      <div className="home-actions-grid">
+        <Link to="/games/new" className="home-action-button">
+          Nova hra
+        </Link>
+        <Link to={`/users/${userId}/history`} className="home-action-button">
+          Historie her
+        </Link>
+      </div>
+    </div>
+  );
 }
